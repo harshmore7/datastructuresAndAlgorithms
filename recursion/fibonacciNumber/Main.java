0f30d8ca75e43d6efb8d2b5d3b2563070bd28c09
Harsh More <harshvmore1@gmail.com>
2025-06-30 11:10:11 +0530

    fibonacci number

--- recursion/fibonacciNumber/Main.java
+++ recursion/fibonacciNumber/Main.java
@@ -3,7 +3,7 @@ public class Main {
         System.out.println(fibOfN(5));
     }
     public static int fibOfN(int n){
-        if(n < 2) return n;
+        if(n <= 1 ) return n;
         return fibOfN(n-1) + fibOfN(n-2);
     }
 }
